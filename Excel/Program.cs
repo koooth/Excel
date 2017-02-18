a76c3d4096157521d7c419b69dea94aa5f1c97a1
admin <admin@PC>
2017-02-19 01:50:10 +0300

    Add project files.

---
+++ Excel/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using System.Diagnostics;
+
+namespace Excel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Microsoft.Office.Interop.Excel.Application _excelApp = new Microsoft.Office.Interop.Excel.Application();
+            _excelApp.Visible = true;
+
+            List<string> rowValue = new List<string> { };
+            string fileName = "C:\\TestData.xlsx";
+
+            //open the workbook
+            Workbook workbook = _excelApp.Workbooks.Open(fileName,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing, Type.Missing, Type.Missing,
+                Type.Missing, Type.Missing);
+
+            //select the first sheet        
+            Worksheet worksheet = (Worksheet)workbook.Worksheets[1];
+
+            //find the used range in worksheet
+            Range excelRange = worksheet.UsedRange;
+
+            //get an object array of all of the cells in the worksheet (their values)
+            object[,] valueArray = (object[,])excelRange.get_Value(
+                        XlRangeValueDataType.xlRangeValueDefault);
+
+            //access the cells
+            for (int row = 1; row <= worksheet.UsedRange.Rows.Count; ++row)
+            {
+                for (int col = 1; col <= worksheet.UsedRange.Columns.Count; ++col)
+                {
+                    //access each cell
+                    //Debug.Print(valueArray[row, col].ToString());
+                    rowValue.Add(excelRange.Cells[row, col].Value2.ToString());
+                }
+                //Console.WriteLine
+                Debug.Print(rowValue[0] + " - " + rowValue[1] + " - " + rowValue[2]);
+                rowValue.Clear();
+            }
+
+            //Console.Read();
+
+            //clean up stuffs
+            workbook.Close(false, Type.Missing, Type.Missing);
+            System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+
+            _excelApp.Quit();
+            System.Runtime.InteropServices.Marshal.FinalReleaseComObject(_excelApp);
+
+
+        }
+    }
+}
